1e3b76a652c61a8f949e3be00a5b6ee386b4537b
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2024-03-19 10:45:31 +0700

    ..

---
+++ main.js
@@ -0,0 +1,10 @@
+function changeTitle(tittle) {
+    const contents = document.querySelectorAll('.contents');
+    contents.forEach(contents => {
+        convert.classList.remove("active");
+        if (convert.classList.remove("active")) {
+            convert.classList.add("active")
+        }
+    }
+    )
+}
