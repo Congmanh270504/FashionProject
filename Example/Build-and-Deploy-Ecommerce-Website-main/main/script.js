8b98aabb152e7112d826d55288bca625b868c227
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2024-04-13 23:16:25 +0700

    update

--- Example/Build-and-Deploy-Ecommerce-Website-main/main/script.js
+++ Example/Build-and-Deploy-Ecommerce-Website-main/main/script.js
@@ -1,6 +1,8 @@
 // Script for navigation bar
 console.log(window.innerWidth);
 console.log(window.innerHeight);
+
+const scrollbutton = document.getElementById("scollbutton");
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
@@ -14,4 +16,20 @@ if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
     })
+}
+window.onscroll = () => {
+    scroll();
+};
+function scroll() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        scrollbutton.style.display = "block";
+    }
+    else {
+        scrollbutton.style.display = "none";
+    }
+}
+function scollToTop() {
+
+    window.scrollTo(0, 0);
+
 }
