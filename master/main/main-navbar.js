05a87e647281544015707f655baff54c1a08d5ca
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2024-05-22 21:32:59 +0700

    #17-update nav bar +search

---
+++ master/main/main-navbar.js
@@ -0,0 +1,6 @@
+const mainNavbar = document.querySelector(".main-navbar");
+// const bar = document.querySelector("#bar");
+
+    bar.addEventListener("click", () => {
+        mainNavbar.classList.toggle("hidden");
+    });
