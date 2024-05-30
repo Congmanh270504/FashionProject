d192f45bfb2cd004a0026701e82e9b0554f0c30b
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2024-05-30 18:16:24 +0700

    #17-update

---
+++ master/main/swiper.js
@@ -0,0 +1,26 @@
+const mySwiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 4,
+            spaceBetween: 20
+        },
+        800: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+        }
+    },
+    direction: "horizontal",
+    spaceBetween: 0,
+})
